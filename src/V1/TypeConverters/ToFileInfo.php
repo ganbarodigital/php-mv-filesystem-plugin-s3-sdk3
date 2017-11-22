caac011d92cfc28ab684c09baad9bfef289fdbcd
Stuart Herbert <stuart@stuartherbert.com>
2017-11-22 15:53:28 +0000

    New: added ToFileInfo type converter

---
+++ src/V1/TypeConverters/ToFileInfo.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Copyright (c) 2017-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   S3Filesystem\V1\TypeConverters
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2017-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://ganbarodigital.github.io/php-mv-filesystem-plugin-s3-sdk3
+ */
+
+namespace GanbaroDigital\S3Filesystem\V1\TypeConverters;
+
+use GanbaroDigital\AdaptersAndPlugins\V1\PluginTypes\PluginClass;
+use GanbaroDigital\MissingBits\ErrorResponders\OnFatal;
+use GanbaroDigital\S3Filesystem\V1\Internal;
+use GanbaroDigital\S3Filesystem\V1\S3FileInfo;
+use GanbaroDigital\S3Filesystem\V1\S3Filesystem;
+
+/**
+ * convert a path into an S3FileInfo value
+ */
+class ToFileInfo implements PluginClass
+{
+    /**
+     * convert a path into an S3FileInfo value
+     *
+     * @param  S3Filesystem $fs
+     *         the filesystem that $path lives on
+     * @param  string|S3FileInfo $path
+     *         the path that we want to convert
+     * @param  OnFatal $onFatal
+     *         what do we do when we can't find $path?
+     * @return S3FileInfo
+     */
+    public static function from(S3Filesystem $fs, string $path, OnFatal $onFatal) : S3FileInfo
+    {
+        // does it need converting?
+        if ($path instanceof S3FileInfo) {
+            // no, it does not
+            return $path;
+        }
+
+        // it needs converting, which means we need to go and learn
+        // about this path
+
+        // TODO - get info from the filesystem contents
+        $contents = $fs->getContents();
+        return Internal\GetFileInfoByPath::from($contents, $path, $onFatal);
+    }
+}
